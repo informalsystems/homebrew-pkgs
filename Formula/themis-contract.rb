70d579aa85b7270ef549150f39bdcdc5bbffa84b
Shon Feder <shon@informal.systems>
2020-12-08 16:05:43 -0500

    Fixes as per brew --audit

--- Formula/themis-contract.rb
+++ Formula/themis-contract.rb
@@ -1,17 +1,17 @@
 class ThemisContract < Formula
-  desc "A command line-based parameterized contracting tool"
+  desc "Command-line tool for parameterized contracting"
   homepage "https://github.com/informalsystems/themis-contract"
   url "https://github.com/informalsystems/themis-contract/archive/v0.2.4.tar.gz"
   sha256 "2955adf0fd7aad65c1b05faeb327b2862e44267654af17f33fe95bffb68881a7"
   license "Apache-2.0"
 
   depends_on "go" => :build
-  depends_on "make" => :build
   depends_on "go-statik" => :build
-  depends_on "pandoc"
-  depends_on "pandoc-crossref"
+  depends_on "make" => :build
   depends_on "dhall-json"
   depends_on "git"
+  depends_on "pandoc"
+  depends_on "pandoc-crossref"
 
   def install
     system "make", "build"
