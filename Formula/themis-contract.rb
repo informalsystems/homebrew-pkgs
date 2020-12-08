9d8bc2f2c861ac1b5218bf799f360c245502c32d
Shon Feder <shon@informal.systems>
2020-12-08 15:34:31 -0500

    Fix formula for themis-contract v0.2.4

--- Formula/themis-contract.rb
+++ Formula/themis-contract.rb
@@ -6,27 +6,19 @@ class ThemisContract < Formula
   license "Apache-2.0"
 
   depends_on "go" => :build
+  depends_on "make" => :build
+  depends_on "go-statik" => :build
   depends_on "pandoc"
   depends_on "pandoc-crossref"
-  depends_on "MacTeX"
   depends_on "dhall-json"
   depends_on "git"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    system "go", "build", *std_go_args
+    system "make", "build"
+    bin.install "build/themis-contract"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test themis-contract`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/themis-contract", "version"
   end
 end
